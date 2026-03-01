e3c227eee7dd3f17bd99090dbfb2ede1c7af626c
Mohd Mustak <mushtakkhan9@gmail.com>
2026-03-01 19:12:09 +0530

    sdfsdf

--- src/components/Works.js
+++ src/components/Works.js
@@ -1,6 +1,8 @@
 import React, { useState, useCallback, useEffect, useRef } from 'react';
 import projectsData from '../data/projects.json';
 import testamentsData from '../data/testaments.json';
+import googlePlayIcon from '../assets/icons/google-play.png';
+import appStoreIcon from '../assets/icons/app-store.png';
 
 // Dynamically import all project images
 const importAll = (r) => {
@@ -251,9 +253,7 @@ const Works = () => {
                               title="Play Store"
                               onClick={(e) => e.stopPropagation()}
                             >
-                              <svg width="18" height="18" viewBox="0 0 24 24" fill="currentColor">
-                                <path d="M3.609 1.814L13.792 12 3.61 22.186a.996.996 0 0 1-.61-.92V2.734a1 1 0 0 1 .609-.92zm10.89 10.893l2.302 2.302-10.937 6.333 8.635-8.635zm3.199-1.207l2.302 1.328-2.302 1.328-1.811-1.328 1.811-1.328zM5.864 3.415l10.937 6.333-2.302 2.302-8.635-8.635z" />
-                              </svg>
+                              <img src={googlePlayIcon} alt="Play Store" width="20" height="20" style={{ objectFit: 'contain' }} />
                             </a>
                           )}
                           {project.iosLink && project.iosLink !== "#" && (
@@ -265,9 +265,7 @@ const Works = () => {
                               title="App Store"
                               onClick={(e) => e.stopPropagation()}
                             >
-                              <svg width="18" height="18" viewBox="0 0 24 24" fill="currentColor">
-                                <path d="M18.71 19.5c-.83 1.24-1.71 2.45-3.05 2.47-1.34.03-1.77-.79-3.29-.79-1.53 0-2 .77-3.27.82-1.31.05-2.3-1.32-3.14-2.53C4.25 17 2.94 12.45 4.7 9.39c.87-1.52 2.43-2.48 4.12-2.51 1.28-.02 2.5.87 3.29.87.78 0 2.26-1.07 3.8-.91.65.03 2.47.26 3.64 1.98-.09.06-2.17 1.28-2.15 3.81.03 3.02 2.65 4.03 2.68 4.04-.03.07-.42 1.44-1.38 2.83M13 3.5c.73-.83 1.94-1.46 2.94-1.5.13 1.17-.34 2.35-1.04 3.19-.69.85-1.83 1.51-2.95 1.42-.15-1.15.41-2.35 1.05-3.11z" />
-                              </svg>
+                              <img src={appStoreIcon} alt="App Store" width="20" height="20" style={{ objectFit: 'contain' }} />
                             </a>
                           )}
                         </div>
